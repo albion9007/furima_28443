41e6770f27977ded045ec163a8f07acb2b8daa87
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-08-13 22:18:25 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -15,6 +16,16 @@ class ApplicationController < ActionController::Base
   # def after_sign_in_path_for(resource)
   #   root_path
   # end
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+  
 
   def after_sign_out_path_for(resource)
     root_path
