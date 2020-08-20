5680268002b65ee20462d76483267ab5595d205c
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-08-20 12:00:24 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show, :search]
+  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show]
 
   def new
